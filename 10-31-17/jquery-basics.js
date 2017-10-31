772cda8b7fbe698a121eb468936590e1d673f0e7
Ryan McGovern <rpmva@udel.edu>
2017-10-31 10:52:01 -0400

    jQuery updated

---
+++ 10-31-17/jquery-basics.js
@@ -0,0 +1,23 @@
+function main () {
+//document.getElementById('google').innerHTML = "Google";
+$('#google').html("Gooooooogle");
+//let links = document.getElementsByClassName('my-link');
+let links = $('.my-link');
+links[0].innerHTML = "Twitter";
+links[0].href = "http://www.twitter.com";
+links[1].innerHTML = "TBD";
+links[1].href = "#";
+
+//hide and show main text
+$('#main-text').hide();
+$('#main-text').fadeIn(5000);
+
+//hide projects
+$('.projects').hide();
+
+$('.my-buttons').click(function () {
+  $(this).next().slideToggle(500);
+})
+}
+
+$(document).ready(main);
